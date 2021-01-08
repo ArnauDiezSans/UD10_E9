5d7a73ba8fffb6c2a0d802aa37bc41af3157f014
Víctor Alfonso Martí <victor19am@gmail.com>
2021-01-08 10:49:03 +0100

    08/01/2021 Cambios de Víctor en la rama de Joan

--- EJERCICIO9/EJERCICIO9/Pelicula.cs
+++ EJERCICIO9/EJERCICIO9/Pelicula.cs
@@ -2,28 +2,52 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Ejercicios.EJ9
+namespace Ejercicios
 {
-    class Pelicula
+    class Cine
     {
-        private string titulo;
-        private int duracion;
-        private int edadMinima;
-        private string director;
+        private Silla[,] sala;
+        private Pelicula reproduccion;
+        private double precio;
 
-        public Pelicula(string titulo, int duracion, int edadMinima, string director)
+        public double Precio { get => precio; set => precio = value; }
+        internal Pelicula Reproduccion { get => reproduccion; set => reproduccion = value; }
+        internal Silla[,] Sala { get => sala; set => sala = value; }
+
+        public Cine(int medida1, int medida2)
         {
-            this.titulo = titulo;
-            this.duracion = duracion;
-            this.edadMinima = edadMinima;
-            this.director = director;
+            int numSilla = 1;
+            int letra = Convert.ToInt32("A");
+            sala = new Silla[medida1,medida2];
+            for (int i = medida1 - 1; i >= 0; i--)
+            {
+                for (int j = 0; i < medida2; i++)
+                {
+                    this.sala[i,j] = new Silla();
+                }
+            }
         }
 
-        public string Titulo { get => titulo; set => titulo = value; }
-        public int Duracion { get => duracion; set => duracion = value; }
-        public int EdadMinima { get => edadMinima; set => edadMinima = value; }
-        public string Director { get => director; set => director = value; }
+        // METODES
+        private Boolean checkPrecioEdad(Espectador espectador, int precio, Pelicula pelicula) 
+        {
+            // comprueba que el espectador tenga dinero y edad suficiente para entrar
+            int edadEspectador = espectador.Edad;
+            double dineroEspectador = espectador.Dinero;
+            int edadMinima = pelicula.EdadMinima;
 
+            Boolean permitido = true;
 
+            if (edadEspectador >= edadMinima || dineroEspectador < precio)
+            {
+                permitido = false;
+            }
+
+            // restamos el precio de la entrada al dinero del espectador
+            espectador.Dinero = espectador.Dinero - precio
+
+            return permitido;
+
+        }
     }
 }
